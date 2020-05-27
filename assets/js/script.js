85747fcb03b8cf1db0df1dab93a5bc7e985aaae1
mhans19 <mdhansen19@gmail.com>
2020-05-27 17:06:31 -0600

    add current date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,28 @@
+// Add Global Variables/Arrays
 var arrayIndicator = 0;
 var includeTimes = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
 
-jQuery.each(includeTimes, 
-function appendElements(){
+// Add Current Date
+$("#currentDay").text(moment().format("dddd, LL"));
 
-$('#timeslot').append(
-    $('<div/>', {'class': 'row'})
-        .append(
-        $('<div/>', {'class': 'col hour'}).append(
-            $('<h2/>', {text: includeTimes[arrayIndicator]})
-        ))
-        .append(
-            $('<div/>', {'class': 'col-9'}).append(
-                $('<p/>', {text: 'description'})
+// Append Schedule Block
+jQuery.each(includeTimes, function appendElements(){
+    $('#timeslot').append(
+        $('<div/>', {'class': 'row'})
+            .append(
+            $('<div/>', {'class': 'col hour'}).append(
+                $('<h2/>', {text: includeTimes[arrayIndicator]})
             ))
-        .append(
-            $('<div/>', {'class': 'col'}).append(
-                $('<button/>', {'class': 'saveBtn'}).append(
-                    $('<span/>', {'class': 'oi oi-check'})
-        )))
-)
-arrayIndicator++;
+            .append(
+                $('<div/>', {'class': 'col-9'}).append(
+                    $('<p/>', {text: 'description'})
+                ))
+            .append(
+                $('<div/>', {'class': 'col'}).append(
+                    $('<button/>', {'class': 'saveBtn'}).append(
+                        $('<span/>', {'class': 'oi oi-check'})
+            )))
+    )
+    arrayIndicator++;
 });
 
