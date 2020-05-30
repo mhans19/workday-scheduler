5c4232e0b6fbe0fa31d90c0189944b3f9e85e031
mhans19 <mdhansen19@gmail.com>
2020-05-29 20:47:17 -0600

    clean code and add comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,19 @@ var arrayIndicator = 0;
 var storageCount = 0;
 var includeTimes = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
 var calTimes = ['09', '10', '11', '12', '13', '14', '15', '16', '17'];
-
 // Add Current Date
 $("#currentDay").text(moment().format("dddd, LL"));
 $("#lastUpdate").text(moment().format("hh:mm A"));
-
 // Append Schedule Block
 jQuery.each(includeTimes, function appendElements(){
     $('#timeslot').append(
-        $('<div/>', {'class': 'row'})
-            .append(
+        $('<div/>', {'class': 'row'}).append(
             $('<div/>', {'class': 'col-12 col-sm-2 col-md-2 col-lg-1 hour'}).append(
-                $('<h2/>', {'class': 'time-block', text: includeTimes[arrayIndicator]})
-            ))
-            .append(
-                $('<div/>', {'id': 'slot' + arrayIndicator, 'class': 'col-12 col-sm-8 col-md-8 col-lg-10 task-group description'}).append(
-                    $('<p/>', {'class': 'form-control textarea'})
-                ))
-            .append(
-                    $('<button/>', {'id': 'saveButton' + includeTimes[arrayIndicator], 'class': 'col-12 col-sm-2 col-md-2 col-lg-1 saveBtn'}).append(
-                        $('<span/>', {'class': 'oi oi-check'})
-            )))
+                $('<h2/>', {'class': 'time-block', text: includeTimes[arrayIndicator]}))).append(
+            $('<div/>', {'id': 'slot' + arrayIndicator, 'class': 'col-12 col-sm-8 col-md-8 col-lg-10 task-group description'}).append(
+                $('<p/>', {'class': 'form-control textarea'}))).append(
+            $('<button/>', {'id': 'saveButton' + includeTimes[arrayIndicator], 'class': 'col-12 col-sm-2 col-md-2 col-lg-1 saveBtn'}).append(
+                $('<span/>', {'class': 'oi oi-check'}))))
 
     var calT = moment(calTimes[arrayIndicator], 'HH');
     if (moment().diff(calT, 'minutes') < 0) {
@@ -33,10 +25,8 @@ jQuery.each(includeTimes, function appendElements(){
     } else {
         $("#slot" + arrayIndicator).addClass("present");
     };
-
     arrayIndicator++;
 });
-
 // Allow text input/change for task
 $(".task-group").on("click", "p", function() {
     var text = $(this).text().trim();
@@ -44,24 +34,21 @@ $(".task-group").on("click", "p", function() {
     $(this).replaceWith(textInput);
     textInput.trigger("focus");
   });
-
 // Refresh page every 10 minutes
 setInterval(function() {
     window.location.reload();
 }, (1000 * 60) * 10);
-
 // Save to Local Storage
 $('.saveBtn').on('click', function(){
+    // Save schedule to local storage
     var calHour = $(this).attr('id');
     var scheduleText = $(this).siblings('div.description').children().val();
-
     localStorage.setItem(calHour, scheduleText);
-})
-
+});
 // Load from Local Storage
 for (var i = 0; i < includeTimes.length; i++){
-$('#slot'+ storageCount).children().text(localStorage.getItem('saveButton' + includeTimes[storageCount]));
-storageCount++;
-}
+    $('#slot'+ storageCount).children().text(localStorage.getItem('saveButton' + includeTimes[storageCount]));
+    storageCount++;
+};
 
 
