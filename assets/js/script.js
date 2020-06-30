478b9f62b02cc8bc00e20083820b946c59ed8375
mhans19 <mdhansen19@gmail.com>
2020-06-30 16:54:48 -0600

    final touches on local storage for new day

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,10 @@ var arrayIndicator = 0;
 var storageCount = 0;
 var includeTimes = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
 var calTimes = ['09', '10', '11', '12', '13', '14', '15', '16', '17'];
+var date = moment().format("dddd, LL");
 // Add Current Date
 $("#currentDay").text(moment().format("dddd, LL"));
+// Add Last Update Time    
 $("#lastUpdate").text(moment().format("hh:mm A"));
 // Append Schedule Block
 jQuery.each(includeTimes, function appendElements(){
@@ -16,7 +18,6 @@ jQuery.each(includeTimes, function appendElements(){
                 $('<p/>', {'class': 'form-control textarea'}))).append(
             $('<button/>', {'id': 'saveButton' + includeTimes[arrayIndicator], 'class': 'col-12 col-sm-2 col-md-2 col-lg-1 saveBtn'}).append(
                 $('<span/>', {'class': 'oi oi-check'}))))
-
     var calT = moment(calTimes[arrayIndicator], 'HH');
     if (moment().diff(calT, 'minutes') < 0) {
         $("#slot" + arrayIndicator).addClass("future");
@@ -44,11 +45,16 @@ $('.saveBtn').on('click', function(){
     var calHour = $(this).attr('id');
     var scheduleText = $(this).siblings('div.description').children().val();
     localStorage.setItem(calHour, scheduleText);
+    localStorage.setItem("date", date);
 });
-// Load from Local Storage
-for (var i = 0; i < includeTimes.length; i++){
-    $('#slot'+ storageCount).children().text(localStorage.getItem('saveButton' + includeTimes[storageCount]));
-    storageCount++;
+// Load from Local Storage if on same day (new day refreshes storage)
+if (localStorage.getItem('date') == moment().format("dddd, LL")) {
+    for (var i = 0; i < includeTimes.length; i++){
+        $('#slot'+ storageCount).children().text(localStorage.getItem('saveButton' + includeTimes[storageCount]));
+        storageCount++;
+    };
+} else {
+    localStorage.clear();
 };
 
 
